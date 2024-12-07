56ed2496721e118796ef8fa922abb628addbd598
Pranav <pranavhp871@gmail.com>
2024-12-07 20:04:45 +0530

    Modified some auth related endpoints

--- src/main/java/com/panic/sasserver/controller/AuthenticationController.java
+++ src/main/java/com/panic/sasserver/controller/AuthenticationController.java
@@ -4,7 +4,12 @@ package com.panic.sasserver.controller;
 import com.panic.sasserver.dto.AuthRequestDTO;
 import com.panic.sasserver.dto.AuthResponseDTO;
 import com.panic.sasserver.dto.RegisterDTO;
+import com.panic.sasserver.model.AppUser;
+import com.panic.sasserver.repository.UserRepository;
 import com.panic.sasserver.service.CustomUserDetailService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import com.panic.sasserver.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -25,6 +27,8 @@ import java.util.Arrays;
 @RequestMapping("/api/auth")
 
 public class AuthenticationController {
+
+
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -38,6 +42,27 @@ public class AuthenticationController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/testtoken") // checks if old token is expired, if not gives a new token to extend session
+    public ResponseEntity<AuthResponseDTO> testRefresh(){ // if it throws a forbidden request, it has expired
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        String newJwt = jwtService.generateToken(userEmail);
+
+        return ResponseEntity.ok(new AuthResponseDTO(newJwt, null));
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/userdata") // fetches user profile data
+    public ResponseEntity<AppUser> getUserData(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        return ResponseEntity.ok(userDetailsService.getUserProfileData(userEmail));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterDTO registerRequest) {
@@ -53,13 +78,13 @@ public class AuthenticationController {
                         .body(new AuthResponseDTO(null, "User Already Exists!!"));
             }
 
-            System.out.println(registerRequest.getPassword());
+
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(registerRequest.getEmail(),
                             unHashedPswd));
 
             final UserDetails userDetails = userDetailsService.loadUserByUsername(registerRequest.getEmail());
-            String jwt = jwtService.generateToken(userDetails.getUsername());
+            String jwt = jwtService.generateToken(userDetails.getUsername()); // using username here because of springboot conventions, actually is email
             return ResponseEntity.ok(new AuthResponseDTO(jwt, null));
 
         }catch(Exception e) {
--- src/main/java/com/panic/sasserver/service/CustomUserDetailService.java
+++ src/main/java/com/panic/sasserver/service/CustomUserDetailService.java
@@ -28,7 +28,7 @@ public class CustomUserDetailService implements UserDetailsService{
         if(user == null) {
             throw new UsernameNotFoundException("User not found with email: " + email);
         }
-        System.out.println(user.getRole().name());
+
         return new org.springframework.security.core.userdetails.User(
                 email, user.getPasswordHash(), Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name())));
     }
@@ -52,4 +52,11 @@ public class CustomUserDetailService implements UserDetailsService{
         return true;
     }
 
+    public AppUser getUserProfileData(String email){
+
+        AppUser user = userRepository.findByEmail(email);
+        user.setPasswordHash("lol");
+
+        return user;
+    }
 }
